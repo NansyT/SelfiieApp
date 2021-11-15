5f75c5afa5d161e982c323f99325f32341db97b7
Nanna Tronbjerg <nanna.tronbjerg@gmail.com>
2021-11-15 10:08:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/selfiieapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.selfiieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Bitmap picture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView.findViewById(R.id.imageView);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        picture = getImage(requestCode, resultCode, data, RESULT_OK);
+        imageView.setImageBitmap(picture);
+    }
+
+    public Bitmap getImage(int requestCode, int resultCode, Intent data, int RESULT_OK) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            return imageBitmap;
+        }
+        else return null;
+    }
+
+}
